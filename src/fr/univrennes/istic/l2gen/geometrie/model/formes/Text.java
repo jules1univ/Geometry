509082268633825be67912d215428aa3a7f22e93
jules1univ <julesgarcia.bzh@gmail.com>
2026-01-28 11:10:12 +0100

    update text

--- src/fr/univrennes/istic/l2gen/geometrie/model/formes/Text.java
+++ src/fr/univrennes/istic/l2gen/geometrie/model/formes/Text.java
@@ -13,51 +13,53 @@ public class Text implements IForme {
 
     @Override
     public String description(int indentation) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'description'");
+        StringBuilder sb = new StringBuilder();
+        sb.append(" ".repeat(Math.max(0, indentation)));
+        sb.append("Text ");
+        sb.append(text);
+        sb.append(" x=");
+        sb.append(this.center.x());
+        sb.append(" y=");
+        sb.append(this.center.y());
+        return sb.toString();
     }
 
     @Override
     public double largeur() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'largeur'");
+        return this.text.length();
     }
 
     @Override
     public double hauteur() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'hauteur'");
+        return 1;
     }
+
 
     @Override
     public Point centre() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'centre'");
+        return this.center;
     }
 
     @Override
     public void deplacer(double dx, double dy) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'deplacer'");
+        this.center.plus(dx, dy);
     }
 
     @Override
     public IForme dupliquer() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'dupliquer'");
+        return new Text(this.center.x(), this.center.y(), this.text);
     }
 
     @Override
-    public void redimensionner(double px, double py) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'redimensionner'");
+    public void redimensionner(double px, double py) {  
+        // Ne rien faire car le texte n'est pas redimensionnable
     }
 
     @Override
     public String enSVG() {
         StringBuilder sb = new StringBuilder();
         sb.append("<text x=\"").append(this.center.x()).append("\" y=\"").append(this.center.y())
-                .append("\" font-family=\"Arial\" font-size=\"12\" fill=\"black\">")
+                .append("\" font-family=\"Arial\" font-size=\"64\" fill=\"black\" text-anchor=\"middle\">")
                 .append(this.text)
                 .append("</text>\n");
 
