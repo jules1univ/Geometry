53d415279696cda08abc4ad18bd0a990538a26aa
GUEMENE Basile <basile.guemene@etudiant.univ-rennes.fr>
2026-01-28 10:06:57 +0100

    Cercle.java (bug?)

---
+++ src/fr/univrennes/istic/l2gen/geometrie/Cercle.java
@@ -0,0 +1,53 @@
+package fr.univrennes.istic.l2gen.geometrie;
+
+public class Cercle implements IForme {
+    private double rayon;
+    private Point centre;
+    
+    public Cercle (double x, double y, double rayon) {
+        this.rayon = rayon;
+        this.centre = new Point(x, y);
+    }
+
+    public Cercle (Point centre, double rayon) {
+        this.centre = centre;
+        this.rayon = rayon;
+    }
+
+    @Override
+    public Point centre(){
+        return this.centre;
+    }
+
+    @Override
+    public String description(int indentation) {
+        return "Centre centre = " + this.centre.x() + "," + this.centre.y() + " r=" + this.rayon;
+    }
+
+    @Override
+    public double hauteur() {
+        return 2*this.rayon;
+    }
+
+    @Override
+    public double largeur() {
+        return 2*this.rayon;
+    }
+
+    @Override
+    public void deplacer(double x, double y) {
+        this.centre = new Point(x, y);
+    }
+
+    @Override
+    public IForme dupliquer() {
+        return new Cercle(this.centre, this.rayon);
+    }
+
+    @Override
+    public void redimensionner(double px, double py) {
+        // Redimensionner un cercle en augmentant le rayon
+        // px et py représentent les facteurs de redimensionnement
+        this.rayon = this.rayon * px;
+    }
+}
