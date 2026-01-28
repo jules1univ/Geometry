5b6e9ea1b020e60569f1fc2a5f0cadf96d7e6b1b
jules1univ <julesgarcia.bzh@gmail.com>
2026-01-28 11:01:36 +0100

    add text class

---
+++ src/fr/univrennes/istic/l2gen/geometrie/model/formes/Text.java
@@ -0,0 +1,58 @@
+package fr.univrennes.istic.l2gen.geometrie.model.formes;
+
+import fr.univrennes.istic.l2gen.geometrie.model.Point;
+
+public class Text implements IForme {
+
+    public Text(double x, double y, String text) {
+    }
+
+    @Override
+    public String description(int indentation) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'description'");
+    }
+
+    @Override
+    public double largeur() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'largeur'");
+    }
+
+    @Override
+    public double hauteur() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'hauteur'");
+    }
+
+    @Override
+    public Point centre() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'centre'");
+    }
+
+    @Override
+    public void deplacer(double dx, double dy) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deplacer'");
+    }
+
+    @Override
+    public IForme dupliquer() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'dupliquer'");
+    }
+
+    @Override
+    public void redimensionner(double px, double py) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'redimensionner'");
+    }
+
+    @Override
+    public String enSVG() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'enSVG'");
+    }
+    
+}
