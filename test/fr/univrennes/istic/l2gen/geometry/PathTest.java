adcdfa2550e9b7fd09bdc2bbe754c9c374a5d131
Marsu2 <elouanbarbier296@gmail.com>
2026-02-11 08:44:13 +0100

    feat: PathTest

--- test/fr/univrennes/istic/l2gen/geometry/PathTest.java
+++ test/fr/univrennes/istic/l2gen/geometry/PathTest.java
@@ -1,35 +1,51 @@
 package fr.univrennes.istic.l2gen.geometry;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 public class PathTest extends AbstractShapeTest<Path> {
     // Test en cours
     @Override
     public Path create() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'create'");
+        Path path = new Path();
+        return path;
+
     }
 
+    @Test
     @Override
     public void testCenter() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'testCenter'");
+        Path path = create();
+        Point center = path.getCenter();
+        Point ExpectedCenter = new Point(0, 0);
+        assertEquals(ExpectedCenter, center);
     }
 
+    @Test
     @Override
     public void testMove() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'testMove'");
+        Path p = create();
+        // le center reste a 0 0 car il n'a pas de bouding box
+        p.move(5, 5);
+        assertEquals(new Point(0, 0), p.getCenter());
     }
 
+    @Test
     @Override
     public void testResize() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'testResize'");
+        Path p = create();
+        p.resize(2, 3);
+        String content = p.getTransform().getContent();
+        assertEquals(true, content.contains("scale("));
     }
 
+    @Test
     @Override
     public void testDescription() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'testDescription'");
+        Path p = create();
+        String expected = "Path D=" + p.draw().getContent();
+        assertEquals(expected, p.getDescription(0));
     }
 
 }
