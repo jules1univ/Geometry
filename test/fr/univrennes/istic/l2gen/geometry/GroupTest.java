49e1c1a797918d30f6e30f13cac6b221af9b74c2
MarcoPaulot <gallon.paul@orange.fr>
2026-02-11 10:07:50 +0100

    fix GroupTest

--- test/fr/univrennes/istic/l2gen/geometry/GroupTest.java
+++ test/fr/univrennes/istic/l2gen/geometry/GroupTest.java
@@ -13,64 +13,95 @@ import java.util.ArrayList;
 
 public class GroupTest extends AbstractShapeTest<Group> {
 
-    Group grpRect = null;
+    Group grp = null;
 
     @Override
     public Group create() {
-        Group grpRect = new Group(new ArrayList<IShape>());
-        return grpRect;
+        Group grp = new Group(new ArrayList<IShape>());
+        return grp;
     }
 
     @Before
     public void testCreate() {
-        grpRect = new Group(new ArrayList<IShape>());
+        grp = new Group(new ArrayList<IShape>());
+        grp.add(new Rectangle());
+        grp.add(new Circle());
+        grp.add(new Ellipse());
+        grp.add(new Line());
+        grp.add(new Polygon());
+        grp.add(new PolyLine());
+        grp.add(new Text());
+        grp.add(new Triangle());
+        grp.add(new Path());
+        grp.add(new Point());
     }
 
     @Test
     public void testAdd() {
-        grpRect.add(new Rectangle());
-        /*
-         * grpRect.add(new Circle());
-         * grpRect.add(new Ellipse());
-         * grpRect.add(new Line());
-         * grpRect.add(new Polygon());
-         * grpRect.add(new PolyLine());
-         * grpRect.add(new Text());
-         * grpRect.add(new Triangle());
-         * grpRect.add(new Path());
-         * grpRect.add(new Point());
-         */
+        grp.add(new Rectangle());
+        grp.add(new Circle());
+        grp.add(new Ellipse());
+        grp.add(new Line());
+        grp.add(new Polygon());
+        grp.add(new PolyLine());
+        grp.add(new Text());
+        grp.add(new Triangle());
+        grp.add(new Path());
+        grp.add(new Point());
     }
 
     @Test
     @Override
     public void testCenter() {
-        assertEquals(0.0, grpRect.getCenter().getX(), 0.1);
+        assertEquals(0.0, grp.getCenter().getX(), 0.1);
     }
 
     @Test
     @Override
     public void testMove() {
-
-        Group grpTest = new Group(new ArrayList<IShape>());
-        grpTest.add(new Rectangle(new Point(1, 1), 0, 0));
-
-        grpRect.move(1.0, 1.0);
-        assertTrue(grpRect.equals(grpTest));
+        grp.move(1.0, 1.0);
+        assertEquals(1.0, grp.getCenter().getX(), 0.2);
+        assertEquals(1.0, grp.getCenter().getY(), 0.2);
     }
 
     @Test
     @Override
     public void testResize() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'testResize'");
+        grp.move(1.0, 1.0);
+        grp.resize(-1.0, -1.0);
+        assertEquals(0.5, grp.getCenter().getX(), 0.2);
+        assertEquals(0.5, grp.getCenter().getY(), 0.2);
+
     }
 
     @Test
     @Override
     public void testDescription() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'testDescription'");
+        String str = grp.getDescription(1);
+        String strAttendu = " Group\n" +
+                "  Rectangle X=0.0 Y=0.0 L=0.0 H=0.0\n" +
+                "  Circle C=0,0 R=0.0\n" +
+                "  Ellipse X=0.0 Y=0.0 RX=0.0 RY=0.0\n" +
+                "Line  X1=0.0 Y1=0.0 X2=0.0 Y2=0.0\n" +
+                "  Polygon POINTS=\n" +
+                "  PolyLine POINTS=\n" +
+                "  Text VALUE= X=0.0 Y=0.0\n" +
+                "  Triangle POINTS= 0.0,0.0 0.0,0.0 0.0,0.0\n" +
+                "  Path D=\n" +
+                "    Point 0,0";
+
+        assertEquals(str, strAttendu);
     }
 
+    @Test
+    public void testGetWidth() {
+        grp.add(new Circle(1.0, 1.0, 0.5));
+        assertEquals(1.0, grp.getCenter().getWidth(), 0.1);
+    }
+
+    @Test
+    public void testGetHeight() {
+        grp.add(new Circle(1.0, 1.0, 0.5));
+        assertEquals(1.0, grp.getCenter().getHeight(), 0.1);
+    }
 }
