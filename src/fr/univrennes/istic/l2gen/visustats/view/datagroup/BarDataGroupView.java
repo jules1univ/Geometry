3af3c2760a7385ccd2bc0e3cda94600219c8fb57
nberthelier <noe.berthelier@gmail.com>
2026-02-18 09:44:35 +0100

    feat: update bar data

--- src/fr/univrennes/istic/l2gen/visustats/view/datagroup/BarDataGroupView.java
+++ src/fr/univrennes/istic/l2gen/visustats/view/datagroup/BarDataGroupView.java
@@ -1,19 +1,29 @@
 package fr.univrennes.istic.l2gen.visustats.view.datagroup;
 
 import fr.univrennes.istic.l2gen.geometry.Point;
+import fr.univrennes.istic.l2gen.svg.interfaces.tag.SVGTag;
 import fr.univrennes.istic.l2gen.visustats.data.DataGroup;
+import fr.univrennes.istic.l2gen.visustats.data.DataSet;
+import fr.univrennes.istic.l2gen.visustats.view.dataset.BarDataSetView;
 
+@SVGTag("g")
 public class BarDataGroupView extends AbstractDataGroupView {
 
     protected double maxHeight;
     protected double spacing;
     protected Point centre;
+    private double barWidth;
 
-    public BarDataGroupView(DataGroup data, double spacing, double maxHeight, Point centre) {
+    public BarDataGroupView(
+            DataGroup data,
+            double spacing,
+            double maxHeight,
+            double barWidth, Point centre) {
         super(data, spacing);
         this.maxHeight = maxHeight;
         this.spacing = spacing;
         this.centre = centre;
+        this.barWidth = barWidth;
     }
 
     @Override
@@ -29,9 +39,16 @@ public class BarDataGroupView extends AbstractDataGroupView {
         // 0 <=> maxHeight;
 
         // axis X=> 0 <=> (spacing + width(dataset)) * nb_dataset
-
+        double width = 0.f;
         for (int i = 0; i < data.size(); i++) {
+            BarDataSetView barview = new BarDataSetView(new Point(
+                    centre.getX() + (spacing + width) * i,
+                    centre.getY()), barWidth, maxHeight);
+            barview.setData(data.get(i));
 
+            width = barview.getWidth();
+
+            this.elements.add(barview);
         }
 
         /*
@@ -40,9 +57,6 @@ public class BarDataGroupView extends AbstractDataGroupView {
          * y
          * )
          */
-
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'update'");
     }
 
 }
